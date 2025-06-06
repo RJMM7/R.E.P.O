eb26f8dc295da268dab0e4875d151044a9bb1e4c
Rodrigo Medrano <00148625@uca.edu.sv>
2025-06-05 21:23:36 -0600

    Primer archivo de do-while

---
+++ Tema3_While_DoWhile/Counter from 1 to 10/counter-from-1-to-10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int numero = 1;
+
+    while (numero <= 10)
+    {
+        cout << numero << " ";
+        numero++;
+    }
+
+    return 0;
+}
